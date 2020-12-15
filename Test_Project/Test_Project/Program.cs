2bebb99ca6461cff589638fa1b4fb301e7a5a87e
Nayzorr <nazarkiriliuk3@gmail.com>
2020-12-15 17:36:27 +0200

    Add Fill_Array Method

--- Test_Project/Test_Project/Program.cs
+++ Test_Project/Test_Project/Program.cs
@@ -10,6 +10,28 @@ namespace Test_Project
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите размер массива: ");
+            int N = int.Parse(Console.ReadLine());
+            int[] arr = new int[N];
+            Fillarr(arr);
+            OutArr(arr);
+        }
+        static void Fillarr(int []arr)
+        {
+            Random rnd = new Random();
+            for(int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(1, 10);
+            }
+        }
+        static void OutArr(int[] arr)
+        {
+            Array.Sort(arr);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(" "+arr[i]); 
+            }          
+            Console.WriteLine();
         }
     }
 }
